7bee531d58e39dd71666a8ddc26194e6803f54f4
Jose <gpjosealberto13@gmail.com>
2022-05-10 16:42:35 +0200

    redo viperc

--- pyper/viper_interface/viper_classes.py
+++ pyper/viper_interface/viper_classes.py
@@ -6,7 +6,7 @@ import json
 import os
 
 from ..io.viper_crc16 import viper_crc16
-from ..io.io_utils import extract_data_from_frame
+from pyper.io import io_utils
 
 
 class PolhemusViper:
@@ -69,12 +69,14 @@ class PolhemusViper:
         msg = array.array("B", command_sequence_list + crc16_list)
         return msg
 
-    def _write_and_read(self, msg):
+    def _write_and_read(self, msg, continuous=False):
         """
         Write a message to the Polhemus Viper and read from the endpoint. This function should not be called directly.
         """
-        self.dev.write(0x02, msg, 200)
+        if not continuous:
+            self.dev.write(0x02, msg, 200)
         resp = self.dev.read(self.endpoint, self.conf["max_size"], 200)
+        timestamp = datetime.datetime.now().isoformat()
         resp_list = resp.tolist()
         resp_crc16 = viper_crc16(resp_list[:-4])  # Remove the crc from the end
         resp_crc16_bytes = struct.pack(f"<{len(resp_crc16)}B", *resp_crc16)
@@ -82,7 +84,7 @@ class PolhemusViper:
             print("CRC16 error")
             return None
         else:
-            return resp_list
+            return resp_list, timestamp
 
     def connect(self):
         """
@@ -98,14 +100,14 @@ class PolhemusViper:
         self.dev = dev
         self.endpoint = self.dev[0][(0, 0)][0]
 
-    def get_single_pno(self, pno_mode="standard"):
+    def get_single_pno(self, pno_mode="standard", orientation="euler_degree"):
         """
         Function that reads a single PNO from Polhemus Viper and prints results as df
 
         Parameters
         --------
         pno_mode : str
-            Type of PNO frame to return. Accepts "standard" and "accelerated". "accelerated" also returns the acceleration of the sensors in addition to the position and orientation. Default is "standard"
+            Type of PNO frame to return. Accepts "standard" and "acceleration". "acceleration" also returns the acceleration of the sensors in addition to the position and orientation. Default is "standard"
 
         Returns
         --------
@@ -114,12 +116,24 @@ class PolhemusViper:
         msg = self._construct_message(
             viper_command="cmd_single_pno", cmd_action="cmd_action_get", arg2=pno_mode
         )
-        resp = self._write_and_read(msg)
-
+        resp, timestamp = self._write_and_read(msg)
+        conv_factor = self.conf["conversion_factor"][orientation]
         if resp is None:
             pass
         else:
-            df = extract_data_from_frame(resp)
+            if pno_mode == "standard":
+                df = io_utils.extract_data_from_standard_frame(
+                    resp, pno_mode=pno_mode, timestamp=timestamp
+                )
+            elif pno_mode == "acceleration":
+                print(resp)
+                df = io_utils.extract_data_from_acceleration_frame(
+                    resp,
+                    sampling_mode="single",
+                    orientation=orientation,
+                    conv_factor=conv_factor,
+                    timestamp=timestamp,
+                )
             return df
 
     def start_continuous(self, pno_mode="standard", frame_counting="reset_frames"):
@@ -130,38 +144,61 @@ class PolhemusViper:
             payload=frame_counting,
         )
 
-        resp = self._write_and_read(msg)
+        resp, _ = self._write_and_read(msg)
         if resp is None:
             pass
         else:
             print(resp)
 
-    def read_continuous(self, dev, ep, stop_condition):
+    def read_continuous(self, stop_condition):
         output_list = []
-        output_dict = dict()
-        max_size = 4 + 4 + 8 * 16 + 4
 
         while not stop_condition.is_set():
-            resp = dev.read(ep, max_size, 200)
-            timestamp = datetime.datetime.now().isoformat()
-            resp_list = resp.tolist()
-            resp_crc16 = viper_crc16(resp_list[:-4])  # Remove the crc from the end
-            resp_crc16_bytes = struct.pack(f"<{len(resp_crc16)}B", *resp_crc16)
+            resp, timestamp = self._write_and_read(msg=None, continuous=True)
+            # resp= self.dev.read(self.endpoint, self.conf["max_size"], 200)
+            # timestamp = datetime.datetime.now().isoformat()
+            # resp_list = resp.tolist()
+            # resp_crc16 = viper_crc16(resp_list[:-4])  # Remove the crc from the end
+            # resp_crc16_bytes = struct.pack(f"<{len(resp_crc16)}B", *resp_crc16)
 
-            if resp_crc16_bytes != bytes(resp_list[-4:]):
-                print("CRC16 error")
-                del resp
+            # if resp_crc16_bytes != bytes(resp_list[-4:]):
+            # print("CRC16 error")
+            # del resp
+            if resp is None:
+                pass
             else:
-                output_list.append((timestamp, resp_list))
-                output_dict[timestamp] = resp_list
+                output_list.append((timestamp, resp))
         return output_list
-        # print(resp_list)
 
     def stop_continuous(self):
-        msg = self._construct_message(viper_command="cmd_continuous_pno", cmd_action="cmd_action_reset")
-        resp = self._write_and_read(msg)
+        msg = self._construct_message(
+            viper_command="cmd_continuous_pno", cmd_action="cmd_action_reset"
+        )
+        resp, _ = self._write_and_read(msg)
         if resp is None:
             pass
         else:
             print(resp)
-        
+            print("Continuous streaming stopped")
+
+    def get_units(self):
+        msg = self._construct_message(
+            viper_command="cmd_units", cmd_action="cmd_action_get"
+        )
+        resp, _ = self._write_and_read(msg)
+        if resp is None:
+            pass
+        else:
+            position = [
+                x
+                for x in self.conf["viper_position_units"].items()
+                if io_utils.intlist_to_int_4bytes(resp[-12 : -12 + 4]) in x
+            ]  # Ger position
+            orientation = [
+                x
+                for x in self.conf["viper_orientation_units"].items()
+                if io_utils.intlist_to_int_4bytes(resp[-12 + 4 : -12 + 8]) in x
+            ]  # Get orientation
+            print(
+                f"The current position units are: {position[0][0]}\nThe current orientation units are: {orientation[0][0]}"
+            )
